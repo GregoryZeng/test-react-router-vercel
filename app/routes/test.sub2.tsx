6337f125b0a83ac5a201fdcf0743815ced2e573c
GregoryZeng <gregzeng@qq.com>
2025-08-31 16:30:40 +0800

    add test.sub2

---
+++ app/routes/test.sub2.tsx
@@ -0,0 +1,16 @@
+import type { Route } from "./+types/test.sub2";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export function loader({ context }: Route.LoaderArgs) {
+  return { message: "test.sub2 loader" };
+}
+
+export default function Sub1({ loaderData }: Route.ComponentProps) {
+  return <div>test.sub2 component</div>;
+}
