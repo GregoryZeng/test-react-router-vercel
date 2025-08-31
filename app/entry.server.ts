4e86470f341e5ec6aef7ead519ea76b062fcb01e
GregoryZeng <gregzeng@qq.com>
2025-08-31 17:55:01 +0800

    add entry.server.tsx entry.client.tsx

--- app/entry.server.ts
+++
@@ -1,9 +0,0 @@
-// app/entry.server.tsx
-import type { AppLoadContext } from "react-router";
-import { handleRequest as rrHandle } from "@vercel/react-router/entry.server";
-
-export async function handleRequest(request: Request, context: AppLoadContext) {
-  const res = await rrHandle(request, context);
-  res.headers.set("x-rr-root-hit", "1");
-  return res;
-}
---
+++ app/entry.server.tsx
@@ -0,0 +1,27 @@
+import { handleRequest } from '@vercel/react-router/entry.server';
+import type { AppLoadContext, EntryContext } from 'react-router';
+ 
+export default async function (
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  routerContext: EntryContext,
+  loadContext?: AppLoadContext,
+): Promise<Response> {
+  const nonce = crypto.randomUUID();
+  const response = await handleRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    routerContext,
+    loadContext,
+    { nonce },
+  );
+  response.headers.set(
+    'Content-Security-Policy',
+    `script-src 'nonce-${nonce}'`,
+  );
+  response.headers.set("x-rr-root-hit", "1");
+  
+  return response;
+}
